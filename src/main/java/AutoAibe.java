0a0bec5a07146a4597a4de605f5792b5c293e55b
Karolis Šlipaitis <slipaitis.karolis@gmail.com>
2018-05-05 19:20:24 +0300

    Extracted PartInfoParser

--- src/main/java/AutoAibe.java
+++ src/main/java/AutoAibe.java
@@ -23,31 +23,12 @@ public final class AutoAibe {
                     .map(DocumentExtractor::getDocument)
                     .filter(Optional::isPresent)
                     .map(Optional::get)
-                    .flatMap(AutoAibe::parseData)
+                    .flatMap(PartInfoParser::parseData)
                     .forEach(d -> writeToFile(output, d));
 
         } catch (IOException e) {
             System.err.println("Could not open " + FILE_NAME + " file, due to: " + e.getMessage());
         }
-    }
-
-    private static Stream<String> parseData(Document document) {
-        String name = document.select(".item-inner-right > .label1").text();
-        String priceRaw = document.select(".primary .product-price-raw").text();
-        String priceDiscount = document.select(".primary .product-price-with-discount").text();
-        String count = document.select("#show_item-info2").text();
-
-        final String[] details = new String[]{""};
-
-        document.select(".item.padded")
-                .stream().forEach(i -> details[0] += String.format("\t%s\t%s", i.child(0).text(), i.child(1).text()));
-
-        document.select(".auto-models-title");
-
-        String csvItem = String.format("%s\t%s\t%s\t%s\t%s%s",
-                name, priceRaw, priceDiscount, count, document.location(), details[0]);
-
-        return Stream.of(csvItem);
     }
 
     private static Stream<String> scrapAllLinks(Document page) {
