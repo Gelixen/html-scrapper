ed91cce45cfb7699fe1a5fed9effa63a903aaed6
Karolis Šlipaitis <slipaitis.karolis@gmail.com>
2018-05-06 11:29:56 +0300

    Added suitable auto models parsing

--- src/main/java/AutoAibe.java
+++ src/main/java/AutoAibe.java
@@ -37,7 +37,7 @@ public final class AutoAibe {
 
     private static void writeHeader(PrintStream output) {
         String header = "Prek\u0117\tKaina\tKaina su nuolaida\tKiekis\t" +
-                String.join("\t", PartDetailType.getAllValues()) + "URL\t";
+                String.join("\t", PartDetailType.getAllValues()) + "\tTinkama automobiliams\tURL";
 
         writeToFile(output, header);
     }
--- src/main/java/PartInfoParser.java
+++ src/main/java/PartInfoParser.java
@@ -1,4 +1,5 @@
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.util.Arrays;
 import java.util.stream.Stream;
@@ -20,21 +21,38 @@ public final class PartInfoParser {
         final String[] partDetails = new String[PartDetailType.values().length];
         Arrays.fill(partDetails, "");
 
-        page.select(".item.padded")
-                .stream()
-                .forEach(detail -> {
-                    int detailTypeIndex = PartDetailType.fromText(detail.child(0).text()).ordinal();
-                    partDetails[detailTypeIndex] = detail.child(1).text();
-                });
+        page.select(".item.padded").forEach(detail -> {
+            int detailTypeIndex = PartDetailType.fromText(detail.child(0).text()).ordinal();
+            partDetails[detailTypeIndex] = detail.child(1).text();
+        });
 
-//        page.select(".auto-models-title");
+        String suitableAutoModels = parseSuitableAutoModels();
 
-        String csvItem = String.format("%s%s\t%s",
-                partInfo.toString(), String.join("\t", partDetails), page.location());
-
-        System.out.println(csvItem);
+        String csvItem = String.format("%s%s\t%s\t%s",
+                partInfo.toString(), String.join("\t", partDetails), suitableAutoModels, page.location());
 
         return Stream.of(csvItem);
+    }
+
+    private static String parseSuitableAutoModels() {
+        String joinedLines = "";
+
+        String[] autoTitles = selectFromPage(".auto-models-title");
+        String[] autoValues = selectFromPage(".auto-models-value");
+
+        for (int i = 0; i < autoTitles.length; i++) {
+            joinedLines += String.format("%s: %s<br>", autoTitles[i], autoValues[i]);
+        }
+
+        return joinedLines;
+    }
+
+    private static String[] selectFromPage(String selector) {
+        return page
+                .select(selector)
+                .stream()
+                .map(Element::text)
+                .toArray(String[]::new);
     }
 
     private static String parseName() {
